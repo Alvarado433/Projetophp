0e93e565934dc972e292acc0b19ab0835af47040
Rhaian <alvaradorhaian766@gmail.com>
2025-01-11 00:06:48 -0300

    modifivacap

--- public/index.php
+++ public/index.php
@@ -6,5 +6,21 @@ include "../vendor/autoload.php";  // Ajuste o caminho conforme necessário
 
 use App\Utilidades\Utilidades;
 
+// Usando o método "mensagem" para exibir a mensagem "teste"
 Utilidades::mensagem("teste");
-Utilidades::conta("10","20","*");
+echo "<br>";
+
+// Realizando uma multiplicação (10 * 20)
+Utilidades::conta("10", "20", "*");
+echo "<br>";
+
+// Realizando uma divisão (10 / 20)
+Utilidades::conta("10", "20", "/");
+echo "<br>";
+
+// Realizando uma subtração (10 - 20)
+Utilidades::conta("10", "20", "-");
+echo "<br>";
+
+// Realizando uma soma (10 + 20)
+Utilidades::conta("10", "20", "+");
